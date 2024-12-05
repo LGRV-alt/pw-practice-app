a3be8524b2713c3a5cbe5254058bd691da039d6d
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-12-05 10:23:23 +0000

    testing radio buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -9,5 +9,58 @@ test.describe("Form Layouts page", () => {
     await page.getByText("Forms").click();
     await page.getByText("Form Layouts").click();
   });
-  test("input fields", async ({ page }) => {});
+  test("input fields", async ({ page }) => {
+    const usingTheGridEmailInput = page
+      .locator("nb-card", { hasText: "Using the grid" })
+      .getByRole("textbox", { name: "email" });
+    await usingTheGridEmailInput.fill("test@test.com");
+    await usingTheGridEmailInput.clear();
+    await usingTheGridEmailInput.pressSequentially("test2@test.com", {
+      delay: 500,
+    });
+    // Generic assertion
+    const inputValue = await usingTheGridEmailInput.inputValue();
+    expect(inputValue).toEqual("test2@test.com");
+
+    // Locator assertion
+    await expect(usingTheGridEmailInput).toHaveValue("test2@test.com");
+  });
+
+  test("radio buttons", async ({ page }) => {
+    const usingTheGridForm = page.locator("nb-card", {
+      hasText: "Using the grid",
+    });
+
+    // await usingTheGridForm.getByLabel("Option 1").check({ force: true });
+    await usingTheGridForm
+      .getByRole("radio", { name: "Option 1" })
+      .check({ force: true });
+
+    //   Making a generic assertion
+    const radioStatus = await usingTheGridForm
+      .getByRole("radio", { name: "Option 1" })
+      .isChecked();
+    expect(radioStatus).toBeTruthy();
+
+    // Locator assertion
+    await expect(
+      usingTheGridForm.getByRole("radio", { name: "Option 1" })
+    ).toBeChecked();
+
+    await usingTheGridForm
+      .getByRole("radio", { name: "Option 2" })
+      .check({ force: true });
+
+    expect(
+      await usingTheGridForm
+        .getByRole("radio", { name: "Option 1" })
+        .isChecked()
+    ).toBeFalsy();
+
+    expect(
+      await usingTheGridForm
+        .getByRole("radio", { name: "Option 2" })
+        .isChecked()
+    ).toBeTruthy();
+  });
 });
