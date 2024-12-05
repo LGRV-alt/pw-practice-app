abced8f7901fd748621c5e34a9aed431f16d7946
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-12-05 12:03:34 +0000

    check boxes and radio buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -57,10 +57,39 @@ test.describe("Form Layouts page", () => {
         .isChecked()
     ).toBeFalsy();
 
+    // This is a bit of a shortcut for the generic locators - you can pass the whole argument into the expect method rather than storing it elsewhere
     expect(
       await usingTheGridForm
         .getByRole("radio", { name: "Option 2" })
         .isChecked()
     ).toBeTruthy();
   });
+});
+
+test("checkboxes", async ({ page }) => {
+  await page.getByText("Modal & Overlays").click();
+  await page.getByText("Toastr").click();
+
+  //   force: true needs to be added due to the Visually hidden property on the element
+  //   Using the check method rather than the click method ensures the desiered outcome is always chosen
+  // If the box is already checked then .check wont change that
+  await page
+    .getByRole("checkbox", { name: "Hide on click" })
+    .uncheck({ force: true });
+  await page
+    .getByRole("checkbox", { name: "Prevent arising of duplicate toast" })
+    .check({ force: true });
+
+  const allBoxes = page.getByRole("checkbox");
+  for (const box of await allBoxes.all()) {
+    await box.uncheck({ force: true });
+    expect(await box.isChecked()).toBeFalsy();
+  }
+});
+
+test("Lists and dropdowns", async ({ page }) => {
+  // Selecting the parent (ngx-header) then the child (nb-select)
+  const dropDownMenu = page.locator("ngx-header nb-select");
+
+  await dropDownMenu.click();
 });
