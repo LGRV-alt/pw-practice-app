b9fe520926dbaee0054534253196ff454fd49b62
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-12-18 16:42:49 +0000

    working through advanced playwright
    Using the NPM package Faker to generate random user data

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { PageManager } from "../page-objects/pageManager";
+import { faker } from "@faker-js/faker";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
@@ -16,11 +17,13 @@ test("navigate to form page", async ({ page }) => {
 
 test("paramitized methods", async ({ page }) => {
   const pm = new PageManager(page);
+  const randomFullName = faker.person.fullName();
+  const randomEmail = `${randomFullName.replace(" ", "")}${faker.number.int(1000)}@test.com`;
 
   await pm.navigateTo().formlayoutsPage();
   await pm.onFormLayoutPage().SubmitUsingTheGridFormWithCredentialsAndSelectOption("test@test.com", "Password1", "Option 1");
-  await pm.onFormLayoutPage().SubmitInlineFormWithNameEmailAndCheckbox("Lewis", "test@test.com", false);
-  await pm.navigateTo().datePickerPage();
-  await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(5);
-  await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(10, 20);
+  await pm.onFormLayoutPage().SubmitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, false);
+  // await pm.navigateTo().datePickerPage();
+  // await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(5);
+  // await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(10, 20);
 });
